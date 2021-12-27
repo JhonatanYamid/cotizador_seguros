7adaa52f5612ddfb33b3650a4594561b98bf96c0
Jhonatan <yamidjhonatan@gmail.com>
2021-12-26 21:20:40 -0500

    Commit inicial

---
+++ src/components/Resumen.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {primerMayuscula} from '../helper';
+import styled from '@emotion/styled';
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align:center;
+    background-color: #00838F;
+    color: #FFF;
+    margin-top: 1rem;
+`
+
+
+const Resumen = ({ datos }) => {
+
+    //extraer datos
+    const { marca, year, plan } = datos;
+
+    if (marca === '' || year === '' || plan === '') return null;
+
+    return (
+        <ContenedorResumen>
+            <h2>Resumen de cotización</h2>
+            <ul>
+                <li>Marca: {primerMayuscula(marca)}</li>
+                <li>Plan: {primerMayuscula(plan)}</li>
+                <li>Año del Auto: {year}</li>
+            </ul>
+        </ContenedorResumen>
+    );
+}
+
+export default Resumen;
